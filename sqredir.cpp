7da4c3247fb1272d5dfe7a05df1a39e87ced3a64
Holger Hoffstätte <holger@applied-asynchrony.com>
2017-11-16 02:05:33 +0100

    Use new blocklist object, properly catch exceptions

--- sqredir.cpp
+++ sqredir.cpp
@@ -1,10 +1,10 @@
 
 #include <cerrno>
 #include <cstdlib>
+#include <exception>
 #include <iostream>
 #include <unistd.h>
 
-// Prototypes
 #include "blocklist.h"
 #include "version.h"
 
@@ -17,71 +17,76 @@ static const int REQ_LINE_MAXLENGTH=4096;
 static const string DEFAULT_CONFIG_FILE = "/etc/sqredir.conf";
 
 // Version
-static void version() {
-    cerr << "sqredir " << SQREDIR_VERSION << endl;
+static string version() {
+	return string("sqredir ") + SQREDIR_VERSION;
 }
 
 // Help
 static void usage() {
-    version();
-    cerr << endl
-        << "Usage: sqredir [options]" << endl
-        << endl
-        << "Options:" << endl
-        << "   -f <file>   Specify path to blocklist configuration" << endl
-        << "   -h          Print this help and exit." << endl
-        << "   -v          Print version and exit." << endl
-        << endl;
+	cerr << "\n"
+		<< version() << "\n"
+		<< "\n"
+		<< "Usage: sqredir [options]" << "\n"
+		<< "\n"
+		<< "Options:" << "\n"
+		<< "   -f <file>   Specify path to configuration" << "\n"
+		<< "   -h          Print this help and exit." << "\n"
+		<< "   -v          Print version and exit." << "\n"
+		<< "\n";
 }
 
 
 // Magic happens here
 int main(int argc, char **argv)
 {
-    // path of config file
-    string config_file = DEFAULT_CONFIG_FILE;
+	// path of config file
+	string config_file = DEFAULT_CONFIG_FILE;
 
-    // parse command line arguments
-    int arg = 0;
-    while ((arg = getopt(argc, argv, "f:hv")) != -1) {
-        switch (arg)
-        {
-            case 'f': {
-                config_file = optarg;
-                break;
-            }
-            case 'h': {
-                usage();
-                exit(EXIT_SUCCESS);
-                break;
-            }
-            case 'v': {
-                version();
-                exit(EXIT_SUCCESS);
-                break;
-            }
-            default: {
-                usage();
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
+	// parse command line arguments
+	int arg = 0;
+	while ((arg = getopt(argc, argv, "f:hv")) != -1) {
+		switch (arg)
+		{
+			case 'f': {
+				config_file = optarg;
+				break;
+			}
+			case 'h': {
+				usage();
+				exit(EXIT_SUCCESS);
+				break;
+			}
+			case 'v': {
+				cout << version() << "\n";
+				exit(EXIT_SUCCESS);
+				break;
+			}
+			default: {
+				usage();
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 
-    // read config file
-    if (!read_config(config_file)) {
-        exit(EXIT_FAILURE);
-    }
+	try
+	{
+		// read config file
+		blocklist bl(config_file);
 
-    // input buffer for a single request
-    char input[REQ_LINE_MAXLENGTH] = {0};
+		// input buffer for a single request
+		char input[REQ_LINE_MAXLENGTH] = {0};
 
-    // loop until EOF from stdin
-    while(fgets(input, REQ_LINE_MAXLENGTH-1, stdin) != NULL) {
-        match_and_reply(input, stdout);
-        fflush(stdout);
-    }
+		// loop until EOF from stdin
+		while(fgets(input, REQ_LINE_MAXLENGTH-1, stdin) != NULL) {
+			bl.match_and_reply(input, stdout);
+			fflush(stdout);
+		}
 
-    // EOF after 'squid -k reconfigure' or shutdown
-    exit(EXIT_SUCCESS);
+		// exit on EOF after 'squid -k reconfigure' or shutdown
+	}
+	catch (exception& ex) {
+		cout << ex.what() << "\n";
+		exit(EXIT_FAILURE);
+	}
 }
 
